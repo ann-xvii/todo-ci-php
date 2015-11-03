3783068720f9acdd5b608678a6a7b6a6fbac2bc3
Anna Atiba <aatiba@crowdrise.com>
2015-11-02 19:36:33 -0800

    update styles, header, footer

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title><?php echo $title ?> | Crowdrise</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<title>ToDo List!</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.css'); ?>">
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500,100italic,300italic,400italic' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>">
 	
 </head>
 <body class="container" style="font-family: Roboto, sans-serif;">
